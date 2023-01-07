39ca4e68a5426cc533d7cccdcbc14fd865fb3903
Brooke Seiter <bseiter@ucsd.edu>
2023-01-07 01:10:13 -0800

    Directions circle made invisible to user

--- front_end/src/Pages/CreateRoutePage.js
+++ front_end/src/Pages/CreateRoutePage.js
@@ -199,6 +199,7 @@ export default function CreateRoutePage () {
               <CircleF 
                 center={routeCenter} 
                 radius={routeRadius} 
+                visible={false}
               />
               
               { stopsObj && routeCenter &&
@@ -261,35 +262,10 @@ export default function CreateRoutePage () {
           </div>
         }
         
-        {/* <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-          Launch demo modal
-        </button>
-        
-       
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                  ...
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-              </div>
-            </div>
-          </div> */}
 
 
-
- 
-
-{/* modal start */}
-       <div
+      {/* modal start */}
+      <div
         id="small_modal"
         className="modal fade"
         role="dialog"
@@ -336,11 +312,6 @@ export default function CreateRoutePage () {
       >
         Get Directions
       </button>
- 
-
-
-   
-        {/* <div id="panel"></div> */}
       </div>
   );
 };
@@ -352,7 +323,7 @@ const Directions = props => {
 
   const options = {
     polylineOptions: {
-      strokeWeight: 6,
+      strokeWeight: 4,
       strokeOpacity: 0.8
     }
   };
