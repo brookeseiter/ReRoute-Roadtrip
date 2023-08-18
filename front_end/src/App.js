9902b29f5edc67fef307955f8e322155d40f726d
Brooke Seiter <bseiter@ucsd.edu>
2023-08-18 11:28:29 -0700

    login triggers correct routes login, login-status, reviews, stops.

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -24,12 +24,10 @@ function App() {
       .then((loginStatusData) => {
         if (loginStatusData.message === 'User is logged in.') {
           console.log('login-status if', isLoggedIn);
-          // setIsLoggedIn(true);
           setCurrentUser(loginStatusData.userId);
           navigate('/profile');
         } else {
           console.log('login-status else', isLoggedIn);
-          // setIsLoggedIn(false);
         }
       })
       .catch(error => {
@@ -54,76 +52,6 @@ function App() {
 
 export default App;
 
-// 8/16 2:51
-// import { Route, Routes } from 'react-router-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import Homepage from './Pages/Homepage';
-// import ProfilePage from './Pages/ProfilePage';
-// import CreateAccountPage from './Pages/CreateAccountPage';
-// import CreateStopPage from './Pages/CreateStopPage';
-// import CreateRoutePage from './Pages/CreateRoutePage';
-// import AllStopsPage from './Pages/AllStopsPage';
-// import StopDetails from './Pages/StopDetailsPage';
-// import NotFound from './Components/NotFound';
-// import { useEffect, useState } from 'react';
-
-
-// function App() {
-//   const[isLoggedIn, setIsLoggedIn] = useState(false);
-//   // const[currentUser, setCurrentUser] = useState({ userId: '', email: '', username: '', phoneNum: '' });
-//   const[currentUser, setCurrentUser] = useState({});
-
-//   useEffect(() => {
-//     fetch(`/login-status`)
-//       .then((response) => response.json())
-//       .then((loginStatusData) => {
-//         if ( loginStatusData.userId) {
-//           setCurrentUser({...currentUser, userId: loginStatusData.userId, email: loginStatusData.email, username: loginStatusData.username, phoneNum: loginStatusData.phoneNum });
-//           console.log('in app else');
-//         }
-//         else {
-//           setIsLoggedIn(false);
-//           setCurrentUser({...currentUser, userId: '', email: '', username: '', phoneNum: '' });
-//           console.log('in app if');
-//         }
-//         // if (loginStatusData.message === 'There is no user currently in the session.') {
-//         //   setIsLoggedIn(false);
-//         //   setCurrentUser({...currentUser, userId: '', email: '', username: '', phoneNum: '' });
-//         //   console.log('in app if');
-//         // }
-//         // else {
-//         //   setCurrentUser({...currentUser, userId: loginStatusData.userId, email: loginStatusData.email, username: loginStatusData.username, phoneNum: loginStatusData.phoneNum });
-//         //   console.log('in app else');
-//         // }
-//       })
-//       .catch(error => {
-//           console.log('error: ', error);
-//       }, []); 
-//   // eslint-disable-next-line
-//   }, [isLoggedIn]);
-
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Homepage  currentUser={currentUser} 
-//                                           setCurrentUser={setCurrentUser} 
-//                                           isLoggedIn={isLoggedIn} 
-//                                           setIsLoggedIn={setIsLoggedIn} />} /> 
-//       <Route path="/create-account" element={<CreateAccountPage />} />
-//       <Route path="/profile" element={<ProfilePage  currentUser={currentUser} 
-//                                                     setCurrentUser={setCurrentUser} 
-//                                                     isLoggedIn={isLoggedIn} 
-//                                                     setIsLoggedIn={setIsLoggedIn} />} />
-//       <Route path="/create-stop" element={<CreateStopPage currentUser={currentUser} 
-//                                                           isLoggedIn={isLoggedIn} />} />
-//       <Route path="/create-route" element={<CreateRoutePage />} />
-//       <Route path="/stops" element={<AllStopsPage />} />
-//       <Route path="/stops/:stop_id" element={<StopDetails currentUser={currentUser} />} />
-//       <Route path="*" element={<NotFound />} />
-//     </Routes> 
-//   );
-// }
-
-// export default App;
 
 
 
