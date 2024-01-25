806617b433af10782dcae82d22f42346d80ce1cf
Brooke Seiter <bseiter@ucsd.edu>
2024-01-24 19:58:07 -0800

    refactored homepage background image and login column structure, reduced unnecessary css

--- front_end/src/Pages/Homepage.js
+++ front_end/src/Pages/Homepage.js
@@ -4,29 +4,28 @@ export default function Homepage ({ user, setUser, isLoggedIn, setIsLoggedIn }) 
 
     return ( 
         <div className="homepage">   
-            <div className="row">
-                <div className="col-md-9 has-bg-img">
-                    <img 
-                        src="../Images/rocks.jpeg"
-                        className="bg-img" 
-                        alt=""
-                    />             
-                </div>
-                <div className="col-md-3">
-                    <br />
-                    <img 
-                        src="../Images/logo.png"
-                        className="rounded mx-auto d-block sm-img" 
-                        id="Logo"
-                        alt=""
-                        width="180px"
-                    />  
-                    <h2 className="text-center">ReRoute Roadtrip</h2>
-                    <p className="text-center">Make each stop part of your adventure.</p> 
-                    <LoginPage user={user} setUser= {setUser} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-                </div>
+            <img 
+                src="../Images/rocks.jpeg"
+                className="bg-img" 
+                alt=""
+            />             
+            <div className="col-md-3">
+                <br />
+                <img 
+                    src="../Images/logo.png"
+                    className="rounded mx-auto d-block sm-img" 
+                    id="Logo"
+                    alt=""
+                />  
+                <h2 className="text-center">ReRoute Roadtrip</h2>
+                <p className="text-center">Make each stop part of your adventure.</p> 
+                <LoginPage 
+                    user={user} 
+                    setUser= {setUser} 
+                    isLoggedIn={isLoggedIn} 
+                    setIsLoggedIn={setIsLoggedIn} 
+                />
             </div>
         </div>
     );
 }
- 
