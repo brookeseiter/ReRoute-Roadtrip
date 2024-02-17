e743368479aaa523d34afd0eb6eafa14109262b6
Brooke Seiter <bseiter@ucsd.edu>
2024-02-17 11:43:19 -0800

    stop searchbar styled

--- front_end/src/Pages/AllStopsPage.js
+++ front_end/src/Pages/AllStopsPage.js
@@ -61,12 +61,11 @@ const AllStopsPage = ({
             {loading ? <Spinner animation="border" /> :
                 <div className="all-stops-page-content container">
                     <h1>All Stops</h1>
-                    <label>Search</label>
                     <input 
                         type="text" 
                         className="stop-search" 
                         onChange={e => setQuery(e.target.value)} 
-                        placeholder='Enter the name of a stop'
+                        placeholder='Search for a stop'
                     />
                     {currentStops && <StopList 
                                         filteredStops={currentStops} 
